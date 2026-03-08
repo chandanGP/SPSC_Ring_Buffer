f974913f1b72693544a1a184d9a05a633981e629
chandanGP <gpchandankumar@gmail.com>
2026-03-08 17:05:46 +0000

    gp

---
+++ SPSC_Ring_Buffer.py
@@ -0,0 +1,74 @@
+import threading
+
+
+class SPSC_Ring_Buffer :
+    
+    def __init__(
+        this,
+        buffer_size : int,
+        enable_get_wait : bool = True
+    ) :
+        this.__buffer_size = buffer_size
+        this.__buffer = [
+            None for _ in range(this.__buffer_size)
+        ]
+        
+        this.__last_idx = len(this.__buffer) - 1
+        this.__head_idx = 0
+        this.__tail_idx = 0
+        
+        this.__get_event = threading.Event()
+        
+        if enable_get_wait :
+            this.get = this.__get_with_wait__
+        else :
+            this.get = this.__get_without_wait__
+    
+    def put(
+        this,
+        x
+    ) :
+        if this.__head_idx < this.__last_idx :
+            next_head_idx = this.__head_idx + 1
+        else :
+            next_head_idx = 0
+        
+        if next_head_idx == this.__tail_idx :
+            return False
+        
+        this.__buffer[this.__head_idx] = x
+        this.__head_idx = next_head_idx
+        this.__get_event.set()
+        return True
+    
+    def __get_with_wait__(
+        this
+    ) :
+        while this.__tail_idx == this.__head_idx :
+            this.__get_event.clear()
+            this.__get_event.wait()
+        
+        if this.__tail_idx < this.__last_idx :
+            next_tail_idx = this.__tail_idx + 1
+        else :
+            next_tail_idx = 0
+        
+        x = this.__buffer[this.__tail_idx]
+        this.__tail_idx = next_tail_idx
+        return x
+    
+    def __get_without_wait__(
+        this
+    ) :
+        if this.__tail_idx == this.__head_idx :
+            return None
+        
+        if this.__tail_idx < this.__last_idx :
+            next_tail_idx = this.__tail_idx + 1
+        else :
+            next_tail_idx = 0
+        
+        x = this.__buffer[this.__tail_idx]
+        this.__tail_idx = next_tail_idx
+        return x
+        
